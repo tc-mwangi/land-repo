0de89773c4cfe0ee8defbf520d196d4a1fa3a149
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-20 08:08:55 +0300

    set up manager

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import unittest
 from flask_script import Manager,Server
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
 #Instances of app
 app = create_app('development')
@@ -23,7 +24,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db,User=User)
 
 
 if __name__ == '__main__':
