1101586c93ff681f803d70f0846f05f206d0e383
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-20 07:57:25 +0300

    input configuration settings

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ from flask_script import Manager,Server
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('default')
+#Instances of app
+app = create_app('development')
+app = create_app('testing')
+app = create_app('production')
+
 
 manager = Manager(app)
 migrate = Migrate(app,db)
