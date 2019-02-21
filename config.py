0fbb230fbfeca0eb7f5ce4c9d7fed62faf5ca34e
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-21 11:00:01 +0300

    comment out config

--- config.py
+++ config.py
@@ -1,45 +1,47 @@
-import os
+# import os
 
-class Config:
+# class Config:
 
-    MAP_API_BASE_URL =''
-    MAP_API_KEY = os.environ.get('MAP_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+#     MAPBOX_API_BASE_URL ='https://api.mapbox.com/directions/v5/{profile}/{coordinates}?access_token={your_access_token}'
+#     # "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token=pk.eyJ1Ijoic2FiZXJkYW5nZXIiLCJhIjoiY2pzYnRwam1oMGRyczQ1bWxweHl4MmltdSJ9.r3PTm_k0VkCSoLtPftuwjg"
+#     MAPBOX_API_KEY = os.environ.get('MAP_API_KEY')
+#     MAPBOX_ACCESS_TOKEN =os.environ.get('MAPBOX_ACCESS_TOKEN')
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-    #  email configurations
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Vybe'
-    SENDER_EMAIL = 'saber.dangermouse@gmail.com'
+#     #  email configurations
+#     MAIL_SERVER = 'smtp.gmail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+#     SUBJECT_PREFIX = 'Vybe'
+#     SENDER_EMAIL = 'saber.dangermouse@gmail.com'
 
-# simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-    @staticmethod
-    def init_app(app):
-        pass
+# # simple mde  configurations
+#     SIMPLEMDE_JS_IIFE = True
+#     SIMPLEMDE_USE_CDN = True
+#     @staticmethod
+#     def init_app(app):
+#         pass
 
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("VYBE_DATABASE_URL")
-    pass
+# class ProdConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("VYBE_DATABASE_URL")
+#     pass
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://saberdanger:cartoonroyalty@localhost/vybetest'
-    pass
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("VYBE_DATABASE_URL")
+#     pass
 
-class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://saberdanger:cartoonroyalty@localhost/vybe'
-    DEBUG = True
+# class DevConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("VYBE_DATABASE_URL")
+#     DEBUG = True
 
-config_options = {
-    "production":ProdConfig,
-    "development":DevConfig,
-    "testing":TestConfig
-    }
+# config_options = {
+#     "production":ProdConfig,
+#     "development":DevConfig,
+#     "testing":TestConfig
+#     }
 
--- manage.py
+++ manage.py
@@ -1,33 +1,33 @@
-import unittest
-from flask_script import Manager,Server
-from app import create_app,db
-from  flask_migrate import Migrate, MigrateCommand
-from config import config_options
-from app.models import User
+# import unittest
+# from flask_script import Manager,Server
+# from app import create_app,db
+# from  flask_migrate import Migrate, MigrateCommand
+# from config import config_options
+# from app.models import User
 
-#Instances of app
-app = create_app('development')
-app = create_app('testing')
-app = create_app('production')
-
-
-manager = Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('server', Server)
-manager.add_command('db',MigrateCommand)
-
-@manager.command
-def test():
-    """Run the unit tests."""
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# #Instances of app
+# app = create_app('development')
+# # app = create_app('testing')
+# # app = create_app('production')
 
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User=User)
+# manager = Manager(app)
+# migrate = Migrate(app,db)
+# manager.add_command('server', Server)
+# manager.add_command('db',MigrateCommand)
+
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-if __name__ == '__main__':
-    manager.run()
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app,db = db,User=User)
+
+
+# if __name__ == '__main__':
+#     manager.run()
 
