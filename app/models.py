afd627f9e65f11e677c3fa86695994d93f122f07
ryan <you@example.com>
2019-02-21 09:24:17 +0300

    add models.py

--- app/models.py
+++ app/models.py
@@ -33,7 +33,7 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Map:
-    '''creates insatnces of map
+class review:
+    '''creates instances of a review
     '''
 
